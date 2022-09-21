5beba2aed66f0410f4e398419543e147bedeb2ba
Matthew Davis <matc.davis@gmail.com>
2022-09-21 19:39:52 +0000

    seed teachers and assistance requests

--- 2_queries_joins/2_total_time_on_assignment_by_cohort.sql
+++ 2_queries_joins/2_total_time_on_assignment_by_cohort.sql
@@ -3,3 +3,12 @@ FROM assignment_submissions
 JOIN students ON students.id = student_id 
 JOIN cohorts ON cohorts.id = cohort_id 
 WHERE cohorts.name = 'FEB12';
+
+
+-- testing HAVING
+-- SELECT students.name, count(assignment_submissions.*) as total_submissions
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- WHERE students.end_date is NULL
+-- GROUP BY students.name
+-- HAVING count(assignment_submissions.*) < 100;
