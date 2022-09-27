2146a2acbb2d7153cec390dfd7f4a6fa7a705a06
Matthew Davis <matc.davis@gmail.com>
2022-09-27 22:34:08 +0000

    update student and teachers to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,19 +1,26 @@
 
 const {pool} = require("./pool");
 const input = process.argv.splice(2)
-// console.log(input[0], input[1]);
+console.log(input);
 
-pool.query(`
-SELECT students.id AS students_id, students.name AS name, cohorts.name AS cohort
-FROM students
-JOIN cohorts ON cohorts.id = students.cohort_id 
-WHERE cohorts.name LIKE '%${input[0]}%'
-LIMIT ${input[1]};
-`)
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+  const cohortName = process.argv[2] || 'FEB';
+  const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
   console.log(res.rows);
   for (const ele of res.rows) {
-    console.log(`${ele.name} has an id of ${ele.students_id} and was in the ${ele.cohort} cohort`)
+    console.log(`${ele.name} has an id of ${ele.student_id} and was in the ${ele.cohort} cohort`)
   }
 })
 .catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,19 +1,24 @@
 const {pool} = require("./pool")
 const input = process.argv.splice(2);
 
-pool.query(`
-  SELECT teachers.name as teacher, cohorts.name as cohort
-  FROM assistance_requests
-  JOIN students ON students.id = assistance_requests.student_id
-  JOIN cohorts ON cohorts.id = students.cohort_id
-  JOIN teachers ON teachers.id = assistance_requests.teacher_id
-  WHERE cohorts.name = '${input[0] || 'JUL02'}'
-  GROUP BY teacher, cohort
-  ORDER BY teacher
-`)
+const queryString = `
+SELECT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+JOIN teachers ON teachers.id = assistance_requests.teacher_id
+WHERE cohorts.name = $1
+GROUP BY teacher, cohort
+ORDER BY teacher
+`;
+
+const cohortDate = input[0] || 'JUL02';
+const values =[`${cohortDate}`]
+
+
+pool.query(queryString, values)
 
   .then(res => {
-    // console.log(res.rows)
     console.log(`Connected, searching DB...`)
     res.rows.forEach(x => {
       console.log(`${x.cohort}: ${x.teacher}`)
